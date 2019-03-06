0cb74ba792f6bec71dd6ea36db5796dad7016e9f
EdisonKon <huainain_cl@126.com>
2019-03-06 16:10:58 +0800

    test

--- SparkDemoByScala/src/main/java/MainTest.scala
+++ SparkDemoByScala/src/main/java/MainTest.scala
@@ -1,9 +1,13 @@
 import scala.collection.mutable.ArrayBuffer
 
 object MainTest {
+  //定长
   var numberArray = new Array[Int](10)
   var sreArray = new Array[String](10)
+  //非定长(buffer)
   var strArrayBuffer = new ArrayBuffer[String]()
+  var intArrayVar=ArrayBuffer(1,1,2)
+
   def main(args: Array[String]): Unit = {
 //    for(a <- numberArray; b <- sreArray) println(a+b)//嵌套循环 100个
 //    for((a,b) <- (numberArray zip sreArray)) println(a+b)//组合循环 10个
@@ -14,5 +18,11 @@ object MainTest {
     strArrayBuffer.foreach(a =>println(a))
     strArrayBuffer.trimEnd(3)//删除后三个
     strArrayBuffer.foreach(a =>println(a))
+    intArrayVar.insert(0,6)//在第0个位置插入6,其余元素往后移
+    intArrayVar.insert(0,7,8,9)//在数组索引为0的位置插入元素7,8,9
+    intArrayVar.remove(0,4)//从索引0开始，删除4个元素
+    intArrayVar.toArray //转成定长数组
+    intArrayVar.toBuffer //转成非定长
+
   }
 }
