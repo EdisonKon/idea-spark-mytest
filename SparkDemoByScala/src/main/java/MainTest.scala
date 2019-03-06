6d4703b55c10b6bd99b6672a668580d4aaea1708
EdisonKon <huainain_cl@126.com>
2019-03-06 16:04:24 +0800

    test

--- SparkDemoByScala/src/main/java/MainTest.scala
+++ SparkDemoByScala/src/main/java/MainTest.scala
@@ -7,12 +7,12 @@ object MainTest {
   def main(args: Array[String]): Unit = {
 //    for(a <- numberArray; b <- sreArray) println(a+b)//嵌套循环 100个
 //    for((a,b) <- (numberArray zip sreArray)) println(a+b)//组合循环 10个
-    strArrayBuffer+=("a","b","c")
+    strArrayBuffer+=("a","b","c")//添加单个/多个元素
     strArrayBuffer.foreach(a =>println(a))
-    strArrayBuffer++=Array("Wllcome","To","XueTuWuYou")
-    strArrayBuffer++=List("Wllcome2","To2","XueTuWuYou2")
+    strArrayBuffer++=Array("Wllcome","To","XueTuWuYou")//添加一个array
+    strArrayBuffer++=List("Wllcome2","To2","XueTuWuYou2")//添加一个list
     strArrayBuffer.foreach(a =>println(a))
-    strArrayBuffer.trimEnd(3)
+    strArrayBuffer.trimEnd(3)//删除后三个
     strArrayBuffer.foreach(a =>println(a))
   }
 }
